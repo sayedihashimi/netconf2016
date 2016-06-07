5a9b51215a1e262318902dc67a873e8ad8462b8d
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-06-07 00:11:32 -0700

    temp

--- appveyor.ps1
+++ appveyor.ps1
@@ -8,7 +8,8 @@ $scriptDir = Get-ScriptDirectory
 $env:NODE_PATH="$env:APPDATA\npm\node_modules"
 $env:path+=";${env:ProgramFiles(x86)}\Microsoft Visual Studio 14.0\Web\External"
 $env:path+=";${env:ProgramFiles(x86)}\Microsoft Visual Studio 14.0\Web\External\git"
-
+npm install gulp -g
+npm install gulp-util -g
 Set-Location (Join-Path $scriptDir 'samples\src\StarterWeb')
 dotnet restore
 msbuild StarterWeb.xproj /p:DeployOnBuild=$true /p:PublishProfile='SayedStarterWeb - Web Deploy' /p:Username=$env:starterWebPubUsername /p:Password=$env:starterWebPubPassword /p:Configuration=Release
