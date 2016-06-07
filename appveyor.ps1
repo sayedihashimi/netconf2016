2fbb1ae8c0a705ac6ed531c1ce8b8a0db2fdc1fb
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-06-07 00:52:41 -0700

    updating appv script

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,31 +1,11 @@
 
-$thisScriptDir = split-path -parent $MyInvocation.MyCommand.Definition
+$scriptDir = split-path -parent $MyInvocation.MyCommand.Definition
+
 $env:NODE_PATH="$env:APPDATA\npm\node_modules"
 $env:path+=";${env:ProgramFiles(x86)}\Microsoft Visual Studio 14.0\Web\External"
 $env:path+=";${env:ProgramFiles(x86)}\Microsoft Visual Studio 14.0\Web\External\git"
-
-
-$pubTemp = 'C:\temp\publish-out\temp'
-if(-not (Test-Path $pubTemp)){ New-Item -Path $pubTemp -ItemType Directory }
-$projectDir = "$thisScriptDir\samples\src\StarterWeb"
-$packDir = "$pubTemp\$([datetime]::now.Ticks)"
-
-try{
-    Set-Location $projectDir
-    # create temp dir for packout
-    New-Item -Path $packDir -ItemType Directory
-    dotnet publish --output $packDir --configuration Release
-
-    # get username and password from a file outside source control
-    $pubUsername=$env:pubUsername
-    $pubPassword=$env:pubPassword    
-    
-    $pubxmlpath=([System.IO.Path]::GetFullPath('.\Properties\PublishProfiles\SayedStarterWeb - Web Deploy.pubxml'))
-    & '.\Properties\PublishProfiles\SayedStarterWeb - Web Deploy-publish.ps1' -packOutput $packDir -pubProfilePath 'C:\data\mycode\netconf2016\samples\src\StarterWeb\Properties\PublishProfiles\SayedStarterWeb - Web Deploy.pubxml' -publishproperties @{'Username'=$pubUsername}
-}
-finally{
-    Pop-Location
-    if(Test-Path $packDir){
-        Remove-Item -Path $packDir -Recurse -ErrorAction SilentlyContinue | Out-Null
-    }    
-}
+npm install gulp -g
+npm install gulp-util -g
+Set-Location (Join-Path $scriptDir 'samples\src\StarterWeb')
+dotnet restore
+msbuild StarterWeb.xproj /p:DeployOnBuild=true /p:PublishProfile='SayedStarterWeb - Web Deploy' /p:Username=$env:starterWebPubUsername /p:Password=$env:starterWebPubPassword /p:Configuration=Release
